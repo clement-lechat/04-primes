d96a1df39d5e6f7f167589815eeedaba532ae52d
clement-lechat <clement.lechat@edu.esiee.fr>
2024-11-14 08:26:24 +0000

    Exercice 4 : Nombre premier

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from math import sqrt
 #### Fonction secondaire
 def isprime(p):
     """
-    Retourne un booleen qui vaut vrai si l'entier en paramètre est un nombre premier
-
+    Verifie que l'entier passé en argument est un nombre premier
+    Args :
+        p : valeur entiere >= 2
+    Returns:
+        isprime(p) : True or False
     >>> isprime(0)
     False
     >>> isprime(1)
@@ -16,14 +19,12 @@ def isprime(p):
     """   
     if p<=1:
         return False
-
     d = int(sqrt(p))
     for i in range(2,d+1):
         if p%i == 0:
             return False
-            break
+
     return True
-    pass
 
 #### Fonction principale
 
@@ -31,18 +32,16 @@ def isprime(p):
 def main():
 
     # vos appels à la fonction secondaire ici
-
     print(isprime(25))
     print(isprime(41))
     print(isprime(1433))
     print(isprime(19))
-
+    help(isprime)
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
 
     print()
-
 
 if __name__ == "__main__":
     main()
