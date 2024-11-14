7e05cd7f8a4118c9e8002bed82375afad2ac5e95
clement-lechat <clement.lechat@edu.esiee.fr>
2024-11-14 09:09:01 +0100

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from math import sqrt
 
 #### Fonction secondaire
-
-
 def isprime(p):
+    """
+    Retourne un booleen qui vaut vrai si l'entier en paramètre est un nombre premier
 
-    # votre code ici
+    >>> isprime(0)
+    False
+    >>> isprime(1)
+    False
+    >>> isprime(4)
+    False
+    >>> isprime(5)
+    True
+    """   
+    if p<=1:
+        return False
 
+    d = int(sqrt(p))
+    for i in range(2,d+1):
+        if p%i == 0:
+            return False
+            break
+    return True
     pass
 
 #### Fonction principale
@@ -15,6 +31,11 @@ def isprime(p):
 def main():
 
     # vos appels à la fonction secondaire ici
+
+    print(isprime(25))
+    print(isprime(41))
+    print(isprime(1433))
+    print(isprime(19))
 
     for n in range(100):
         if isprime(n):
